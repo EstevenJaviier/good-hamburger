3184eece5685f68d53e29d43b27be9e1dbf6a7ec
EstevenJaviier <estevenjaviier@gmail.com>
2025-12-04 16:32:36 -0500

    se agregó tipado en variable en el test

--- src/app/core/utils/discount.util.spec.ts
+++ src/app/core/utils/discount.util.spec.ts
@@ -1,19 +1,34 @@
+import { Item } from '../models/item.model';
 import { calculateDiscount } from './discount.util';
 
 describe('Discount Utils', () => {
   it('should apply 20% for sandwich + fries + drink', () => {
-    const items = [
-      { type: 'sandwich' } as any,
-      { name: 'Fries' } as any,
-      { name: 'Soft drink' } as any,
+    const items: Item[] = [
+      { type: 'sandwich', name: 'Burger', id: 1, price: 5.0 },
+      { name: 'Fries', id: 2, price: 2.0, type: 'extra' },
+      { name: 'Soft drink', id: 3, price: 2.5, type: 'extra' },
     ];
-    expect(calculateDiscount(items)).toBe(0.20);
+    expect(calculateDiscount(items)).toBe(0.2);
+  });
+
+  it('should apply 15% for sandwich + drink', () => {
+    const items: Item[] = [
+      { type: 'sandwich', name: 'Burger', id: 1, price: 5.0 },
+      { name: 'Soft drink', id: 2, price: 2.5, type: 'extra' },
+    ];
+    expect(calculateDiscount(items)).toBe(0.15);
+  });
+
+  it('should apply 10% for sandwich + fries', () => {
+    const items: Item[] = [
+      { type: 'sandwich', name: 'Burger', id: 1, price: 5.0 },
+      { name: 'Fries', id: 2, price: 2.0, type: 'extra' },
+    ];
+    expect(calculateDiscount(items)).toBe(0.1);
   });
 
   it('should return 0 for no discount', () => {
-    const items = [
-      { type: 'extra', name: 'Fries' } as any,
-    ];
+    const items: Item[] = [{ type: 'extra', name: 'Fries', id: 1, price: 2.0 }];
     expect(calculateDiscount(items)).toBe(0);
   });
 });
